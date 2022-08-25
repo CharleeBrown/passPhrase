dc690b1c823d070b8c4854c9e6df491075feb90b
Coleman <charleebrown22@gmail.com>
2022-08-25 13:36:36 -0500

    asdf

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,9 @@ const { match } = require('assert');
 const hold = new Array();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let main = fs.readFile('./effText.txt',{encoding:'utf8', flag:'r'}, function(err, data){
-      //console.log(getLine);
-      data.split('/\w$\r/').forEach(line =>{
-        hold.push(line);
-      });
-      
-    console.log(hold[4])
-    //console.log(hold[0]);
-  });
-    //console.log(main);
-
-    let phraseCount = 0;
-    //let rng = Math.floor(Math.random() * (data.length-1)+1);
-    //console.log(data + '-' + data.indexOf(data));
+  let tr = fs.readFile('./words.json');
+  console.log(tr);
     
-    //console.log(data);
-  
   res.render('index', { title: 'Express' });
 });
 
